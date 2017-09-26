936a7f88ced982a6e49df6ff1191e59fdaed4af8
khomiakAndriy <khomiak.andriy@gmail.com>
2017-09-26 12:52:10 +0300

    add properties file

---
+++ src/main/java/com/hibernate/controller/ConnectionApp.java
@@ -0,0 +1,25 @@
+package com.hibernate.controller;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class ConnectionApp {
+    public static void main(String[] args) {
+        String jdbcUrl = "jdbc:mysql://localhost:3306/bookstore_db?useSSL=false";
+        String user = "login";
+        String pass = "password";
+
+        try {
+            System.out.println("Connecting to database: " + jdbcUrl);
+
+            Connection myConn =
+                    DriverManager.getConnection(jdbcUrl, user, pass);
+
+            System.out.println("Connection successful!!!");
+
+        }
+        catch (Exception exc) {
+            exc.printStackTrace();
+        }
+    }
+}
---
+++ src/main/java/com/hibernate/controller/HomeController.java
@@ -0,0 +1,32 @@
+package com.hibernate.controller;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+@Controller
+public class HomeController {
+
+    @Autowired
+    private DataSource myDataSource;
+
+    @RequestMapping("/")
+    public String home(){
+
+        try {
+            Connection connection = myDataSource.getConnection();
+            System.out.println("Succsessful connection");
+        } catch (SQLException e) {
+            System.out.println("Connection failed");
+        }
+
+        return "home";
+    }
+}
